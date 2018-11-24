7c3e166cf6e468db6504e10306b2b63f74d2ef07
Ayana Bando <ayanaflax@yahoo.com>
2018-11-24 15:48:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,4 +35,13 @@ function distanceTravelledInFeet(hq, block) {
 function calculatesFarePrice(start, destination){
 let price;
 
+  if (destination <= 400){
+    result = 'This one is on me!';
+  } else if (2000 < feet && feet < 2500){
+    result = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500){
+    result = 'No can do.';
+  }
+  return result
+}
 }
