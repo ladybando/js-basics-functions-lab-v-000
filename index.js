903aa1786045790884641f2ebcea13e56a5189d6
Ayana Bando <ayanaflax@yahoo.com>
2018-11-24 15:39:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ return dist;
 function distanceFromHqInFeet(feet) {
 let dist;
  
-if (feet > 42) {
-  dist =  ((feet - 42) * 264);
-} else if (feet < 42) {
+ if (feet < 42) {
   dist = ((42 - feet) * 264);
 }
 return dist;
